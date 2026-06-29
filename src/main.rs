a641b035602f6f40952b8a3b5353c33efc1d4d89
Sfrfl-17304 <akoujanali38@gmail.com>
2026-06-29 10:36:45 +0100

    fix: changed main from placeholders to calling modules, and tested using a json file

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use clap::Parser;
 mod ast;
 mod cli;
 mod generator;
+
 use cli::{Cli, Commands};
 
 fn main() {
     let cli = Cli::parse();
-
     match cli.command {
-        Commands::Gen(args) => println!("[gen] input={:?} output={:?}", args.input, args.output),
+        Commands::Gen(args) => {
+            let playbook = generator::generate_from_file(args.input.to_str().unwrap()).unwrap();
+            generator::output_file_yaml(playbook, args.output.to_str().unwrap()).unwrap();
+            println!("Playbook generated successfully");
+        }
         Commands::Lint(args) => println!("[lint] playbook={:?}", args.playbook),
         Commands::Run(args) => println!(
             "[run] playbook={:?} inventory={:?} dry_run={}",
